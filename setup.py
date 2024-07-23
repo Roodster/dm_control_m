1195d9977dc2679339618b7bf67841bf7ac65430
Rody Haket <rhaket@student.tudelft.nl>
2024-07-23 20:21:14 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='dm_control_m',
+    version='0.1.0',
+    author='Rody Haket',
+    description=('Custom DeepMind Control Suite'),
+    license='MIT',
+    keywords='gym dm_control openai deepmind',
+    packages=find_packages(),
+    install_requires=[
+        'gym',
+    ],
+)
